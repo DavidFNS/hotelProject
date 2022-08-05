909c03c0405da9705ad53595d2b51dda66c21527
Dovud <foundation240897@gmial.com>
2022-08-05 18:20:58 +0500

    commit BookingFiles

---
+++ src/main/java/hotel/controller/BookingController.java
@@ -0,0 +1,46 @@
+package hotel.controller;
+
+import hotel.dto.BookingDto;
+import hotel.dto.ResponseDto;
+import hotel.entity.Booking;
+import hotel.service.BookingService;
+import hotel.service.impl.BookingServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/Booking")
+
+public class BookingController {
+    private final BookingServiceImpl bookingService;
+
+    public BookingController(BookingServiceImpl bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public ResponseDto<List<BookingDto>> getAllBooking(){
+        return bookingService.getAllBooking();
+    }
+
+    @GetMapping(name = "/{id}")
+    public ResponseDto<BookingDto> getBookingById(@PathVariable Integer id){
+        return bookingService.getBookingById(id);
+    }
+
+    @PostMapping
+    public ResponseDto<BookingDto> addBooking(@RequestBody BookingDto bookingDto){
+        return bookingService.addBooking(bookingDto);
+    }
+
+    @PutMapping
+    public ResponseDto updateBooking(@RequestBody BookingDto bookingDto){
+        return bookingService.updateBooking(bookingDto);
+    }
+
+    @DeleteMapping(name = "/{id}")
+    public ResponseDto deleteBooking(@PathVariable Integer id){
+        return bookingService.deleteBooking(id);
+    }
+}
--- src/main/java/hotel/dto/ResponseDto.java
+++ src/main/java/hotel/dto/ResponseDto.java
@@ -1,10 +1,14 @@
 package hotel.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDto<T> {
     private int code;
     private boolean success;
