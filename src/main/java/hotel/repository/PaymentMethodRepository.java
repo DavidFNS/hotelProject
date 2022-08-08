23f04b906ad1eef0317431acc5cca53b1781a910
siroj0101 <rozmatovsiroj68@gmail.com>
2022-08-08 06:01:40 +0300

    commit bookingFiles

---
+++ src/main/java/hotel/repository/PaymentMethodRepository.java
@@ -0,0 +1,10 @@
+package hotel.repository;
+
+
+import hotel.entity.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Integer> {
+}
